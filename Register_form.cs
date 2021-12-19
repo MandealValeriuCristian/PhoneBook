720b88179ca5ee39d05675e8b2b71c0acfa67838
Mandeal Valeriu Cristian <mandealvaleriucristian@gmail.com>
2021-12-19 20:14:30 +0200

    Write the code to clear the text of all user info TextBoxes

--- Register_form.cs
+++ Register_form.cs
@@ -17,13 +17,23 @@ namespace PhoneBook
             InitializeComponent();
         }
 
-       
+
         private void btnNew_Click(object sender, EventArgs e)
         {
             this.btnNew.Enabled = false;
             this.btnSave.Enabled = true;
             // ----------------------------
             this.Info_group_box.Enabled = true;
+            // ----------Clear TextBoxes------------------
+            this.ID_textBox1.ResetText();
+            this.FirstName_textBox2.ResetText();
+            this.LastName_textBox3.ResetText();
+            this.Tell_textBox4.ResetText();
+            this.Cell_textBox5.ResetText();
+            this.Email_textBox6.ResetText();
+            this.Adress_textBox7.ResetText();
+            this.Comment_textBox8.ResetText();
+            // -------------------------------------------
         }
 
         private void btnSave_Click(object sender, EventArgs e)
@@ -39,6 +49,6 @@ namespace PhoneBook
             this.Info_group_box.Enabled = false;
         }
 
-       
+
     }
 }
