a497e5a99f83803366da01712ede05f7fbc962bf
Mandeal Valeriu Cristian <mandealvaleriucristian@gmail.com>
2021-12-19 20:58:09 +0200

    Design search user Form

--- Search_form.Designer.cs
+++ Search_form.Designer.cs
@@ -29,22 +29,279 @@ namespace PhoneBook
         /// </summary>
         private void InitializeComponent()
         {
+            this.Info_group_box = new System.Windows.Forms.GroupBox();
+            this.Comment_textBox8 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.Adress_textBox7 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Email_textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Cell_textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Tell_textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.LastName_textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.FirstName_textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ID_textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Info_group_box.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // Info_group_box
+            // 
+            this.Info_group_box.Controls.Add(this.Comment_textBox8);
+            this.Info_group_box.Controls.Add(this.label8);
+            this.Info_group_box.Controls.Add(this.Adress_textBox7);
+            this.Info_group_box.Controls.Add(this.label7);
+            this.Info_group_box.Controls.Add(this.Email_textBox6);
+            this.Info_group_box.Controls.Add(this.label6);
+            this.Info_group_box.Controls.Add(this.Cell_textBox5);
+            this.Info_group_box.Controls.Add(this.label5);
+            this.Info_group_box.Controls.Add(this.Tell_textBox4);
+            this.Info_group_box.Controls.Add(this.label4);
+            this.Info_group_box.Controls.Add(this.LastName_textBox3);
+            this.Info_group_box.Controls.Add(this.label3);
+            this.Info_group_box.Controls.Add(this.FirstName_textBox2);
+            this.Info_group_box.Controls.Add(this.label2);
+            this.Info_group_box.Controls.Add(this.ID_textBox1);
+            this.Info_group_box.Controls.Add(this.label1);
+            this.Info_group_box.Location = new System.Drawing.Point(12, 182);
+            this.Info_group_box.Name = "Info_group_box";
+            this.Info_group_box.Size = new System.Drawing.Size(591, 406);
+            this.Info_group_box.TabIndex = 2;
+            this.Info_group_box.TabStop = false;
+            this.Info_group_box.Text = "Information";
+            // 
+            // Comment_textBox8
+            // 
+            this.Comment_textBox8.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.Comment_textBox8.Location = new System.Drawing.Point(107, 353);
+            this.Comment_textBox8.Name = "Comment_textBox8";
+            this.Comment_textBox8.ReadOnly = true;
+            this.Comment_textBox8.Size = new System.Drawing.Size(478, 22);
+            this.Comment_textBox8.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(15, 358);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(71, 17);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Comment:";
+            // 
+            // Adress_textBox7
+            // 
+            this.Adress_textBox7.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.Adress_textBox7.Location = new System.Drawing.Point(107, 309);
+            this.Adress_textBox7.Name = "Adress_textBox7";
+            this.Adress_textBox7.ReadOnly = true;
+            this.Adress_textBox7.Size = new System.Drawing.Size(478, 22);
+            this.Adress_textBox7.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(15, 314);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(56, 17);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Adress:";
+            // 
+            // Email_textBox6
+            // 
+            this.Email_textBox6.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.Email_textBox6.Location = new System.Drawing.Point(107, 263);
+            this.Email_textBox6.Name = "Email_textBox6";
+            this.Email_textBox6.ReadOnly = true;
+            this.Email_textBox6.Size = new System.Drawing.Size(478, 22);
+            this.Email_textBox6.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 268);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Email:";
+            // 
+            // Cell_textBox5
+            // 
+            this.Cell_textBox5.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.Cell_textBox5.Location = new System.Drawing.Point(107, 219);
+            this.Cell_textBox5.Name = "Cell_textBox5";
+            this.Cell_textBox5.ReadOnly = true;
+            this.Cell_textBox5.Size = new System.Drawing.Size(478, 22);
+            this.Cell_textBox5.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 224);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Cell:";
+            // 
+            // Tell_textBox4
+            // 
+            this.Tell_textBox4.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.Tell_textBox4.Location = new System.Drawing.Point(107, 179);
+            this.Tell_textBox4.Name = "Tell_textBox4";
+            this.Tell_textBox4.ReadOnly = true;
+            this.Tell_textBox4.Size = new System.Drawing.Size(478, 22);
+            this.Tell_textBox4.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Tell:";
+            // 
+            // LastName_textBox3
+            // 
+            this.LastName_textBox3.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.LastName_textBox3.Location = new System.Drawing.Point(107, 141);
+            this.LastName_textBox3.Name = "LastName_textBox3";
+            this.LastName_textBox3.ReadOnly = true;
+            this.LastName_textBox3.Size = new System.Drawing.Size(478, 22);
+            this.LastName_textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Last Name:";
+            // 
+            // FirstName_textBox2
+            // 
+            this.FirstName_textBox2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.FirstName_textBox2.Location = new System.Drawing.Point(107, 101);
+            this.FirstName_textBox2.Name = "FirstName_textBox2";
+            this.FirstName_textBox2.ReadOnly = true;
+            this.FirstName_textBox2.Size = new System.Drawing.Size(478, 22);
+            this.FirstName_textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "First Name:";
+            // 
+            // ID_textBox1
+            // 
+            this.ID_textBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ID_textBox1.Location = new System.Drawing.Point(107, 64);
+            this.ID_textBox1.Name = "ID_textBox1";
+            this.ID_textBox1.ReadOnly = true;
+            this.ID_textBox1.Size = new System.Drawing.Size(478, 22);
+            this.ID_textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Location = new System.Drawing.Point(12, 16);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(591, 131);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Search";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(107, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(356, 22);
+            this.textBox1.TabIndex = 17;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(46, 58);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(25, 17);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "ID:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(490, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Search_form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.ClientSize = new System.Drawing.Size(630, 494);
+            this.ClientSize = new System.Drawing.Size(630, 600);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Info_group_box);
             this.MaximizeBox = false;
             this.Name = "Search_form";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Search Data";
+            this.Info_group_box.ResumeLayout(false);
+            this.Info_group_box.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox Info_group_box;
+        private System.Windows.Forms.TextBox Comment_textBox8;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox Adress_textBox7;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox Email_textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox Cell_textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox Tell_textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox LastName_textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox FirstName_textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ID_textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label9;
     }
 }
